33658ad45828cf18bb921484ad758836c9e4ad62
Ajay Thomas <ajaythomas.inc@gmail.com>
2016-09-18 11:23:41 -0400

    Create LCABinaryTree.cpp
    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/

---
+++ LCABinaryTree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if(root == NULL)
+            return NULL;
+            
+        if(root == p || root == q)
+            return root;
+            
+        TreeNode* leftNode = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightNode = lowestCommonAncestor(root->right, p, q);
+        
+        //both p and q were found in different subtrees
+        if(leftNode && rightNode)
+            return root;
+            
+        if(leftNode)
+            return leftNode;
+        else
+            return rightNode;
+    }
+};
